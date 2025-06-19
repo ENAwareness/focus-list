ddd5ed51360b3cd9fcd9018c7633d8eb337a312a
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-06-19 20:55:46 +0900

    Set up userRoutes and User model; added register/login routes and mounted them in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
