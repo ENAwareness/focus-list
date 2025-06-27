0c1ff8a1106c685fa7d3dab5ebb2584a4e208f06
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-06-28 01:39:27 +0900

    fix(server): add validator dependency

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import User from '../models/User.js';
 import validator from 'validator';
-import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const router = express.Router();
